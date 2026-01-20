0171ab391b76eae1f9d8ec71938f4c46a4d0764a
praveen <ageabove60@gmail.com>
2026-01-20 21:27:48 +0530

    created scroll project animation

--- index.js
+++ index.js
@@ -2,16 +2,36 @@
  // use a script tag or an external JS file
  document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger,ScrollSmoother)
+    projects=document.querySelectorAll(".project")
+    images=document.querySelectorAll(".project-image")
+    const totalProjects = projects.length;
+
+
+ScrollTrigger.create({
+  trigger: ".recent-work",
+  start: "top top",
+  end: "+=300%",
+  pin: true,
+  scrub: true,
+  markers: false,
+  onUpdate: self => {
+     const progress = gsap.utils.clamp(0, 0.999, self.progress);
+    const index = Math.floor(progress * projects.length);
+    showProject(index);
+  }
+});
+
+
     gsap.to(".skills",{
         scrollTrigger:{
             trigger:".skills",
             start:"center center",
             end:"+=70%",
             scrub:2,
-            markers:true,
+            markers:false,
             pin:true,
         },
-    })
+    });
     let t1=gsap.timeline();
     t1
     .from(".hero",{
@@ -37,5 +57,19 @@
         y:-20,
         duration: 0.5
     })
+    function showProject(i){
+        projects.forEach((project,index)=>{
+            if (i===index){
+                project.style.opacity="1"
+                images[index].style.visibility="visible"
+            }
+            else{
+            project.style.opacity="0.3"
+            images[index].style.visibility="hidden"
+            }
+        });
+    }
+    ScrollTrigger.refresh();
+
  });
 
